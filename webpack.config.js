ca5ac36382d009fb9943a80dfb43ed23d2d1b2db
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-20 21:28:21 -0300

    Seting up dotenv to use .env files

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,69 @@ const isDev = NODE_ENV === 'development';
 // const isDev = (process.argv[1] || '')
 //					.indexOf('hjs-dev-server') !== -1;
 
-const webpack = require('webpack'), fs = require('fs'), path = require('path'), join = path.join, resolve = path.resolve;
+const webpack = require('webpack'),
+	getConfig = require('hjs-webpack'),
+	fs = require('fs'),
+	path = require('path'),
+	dotenv = require('dotenv');
 
-const getConfig = require('hjs-webpack');
+const join = path.join,
+	resolve = path.resolve;
 
 //Variables to optimize configuration
-const root = resolve(__dirname), src = join(root, 'src'), modules = join(root, 'node_modules'), dest = join(root, 'dist');
+const root = resolve(__dirname),
+	src = join(root, 'src'),
+	modules = join(root, 'node_modules'),
+	dest = join(root, 'dist');
+
+/**
+ * Loading the variables is a pretty simple process using dotenv.
+ * In our webpack.config.js file, let's load up the .env file in our
+ * environment
+ */
+const dotEnvVars = dotenv.config(),
+	// To load these files in our server
+	enviromentEnv = dotenv.config({
+		path: join(root, 'config', `${NODE_ENV}.config.js`),
+		silent: true
+	});
+
+/**
+ * We can merge these two objects together to allow the environment-based 
+ * [env].config.js file to overwrite the global one using Object.assign()
+ */
+const envVariables = Object.assign({}, dotEnvVars, enviromentEnv);
+
+/**
+ * In our webpack.config.js file, let's use the reduce() method 
+ * to create an object that contains conventional values in our source 
+ * with their stringified values
+ */
+const defines = Object.keys(envVariables)
+	.reduce((memo, key) => {
+		const val = JSON.stringify(envVariables[key]);
+
+		memo[`__${key.toUpperCase()}__`] = val;
+
+		return memo;
+	}, {
+		__NODE_ENV__: JSON.stringify(NODE_ENV)
+	});
 
 let config = getConfig({
 	in: join(src, 'app.js'),
 	out: dest,
 	clearBeforeBuild: true
 });
+
+/**
+ * The defines object can now become the configuration object that the 
+ * DefinePlugin() plugin expects to use to replace variables.
+ * We'll prepend the existing webpack plugin list with our DefinePlugin()
+ */
+config.plugins = [
+	new webpack.DefinePlugin(defines)
+].concat(config.plugins);
 
 config.postcss = [].concat([
 	require('precss')({}),
@@ -59,5 +110,7 @@ config.module.loaders.push({
 	include: [modules],
 	loader: 'style!css'
 });
+
+
 
 module.exports = config;
