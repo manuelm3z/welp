bc4c1ef8b2e625b25025490db6b3e6a5ea20ee75
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-20 21:32:56 -0300

    Demo de font awesome

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 import styles from './styles.module.css';
+import 'font-awesome/css/font-awesome.css';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className={styles.wrapper}>
-				<h1>Enviroment: {__NODE_ENV__}</h1>
+				<h1>
+					<i className="fa fa-star"></i>
+					Enviroment: {__NODE_ENV__}
+				</h1>
 			</div>
 			);
 	}
