0e4d0e623d6f5e57ced100165b2333a099dcc906
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-20 22:23:59 -0300

    Another optimization

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,12 @@ module.exports = function(config) {
         exclude: [],
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'tests.webpack.js': [
+                'webpack',
+                'sourcemap'
+            ]
+        },
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -62,7 +67,8 @@ module.exports = function(config) {
             'karma-chai',
             'karma-webpack',
             'karma-phantomjs-launcher',
-            'karma-spec-reporter'
+            'karma-spec-reporter',
+            'karma-sourcemap-loader'
         ]
     });
 };
