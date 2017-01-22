d62be8dd0c67df32de21d4ca615005971fb126ce
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-21 23:53:48 -0300

    Typed mistakes

--- src/utils/googleApiHelpers.js
+++ src/utils/googleApiHelpers.js
@@ -1,8 +1,8 @@
 export function searchNearby(google, map, request) {
 	return new Promise((resolve, reject) => {
-		const service = new google.maps.places.PlaceService(map);
+		const service = new google.maps.places.PlacesService(map);
 
-		serice.nearbySearch(request, (results, status, pagination) => {
+		service.nearbySearch(request, (results, status, pagination) => {
 			if (status == google.maps.places.PlacesServiceStatus.OK) {
 				resolve(results, pagination);
 			} else {
