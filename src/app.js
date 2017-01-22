e7be55723ffb9d6f7129ce970ac7323c3936e01e
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-21 22:35:02 -0300

    Passing test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.css';
+import './app.css';
+
+import { browserHistory, Router, Route } from 'react-router';
+
 import App from 'containers/App/App';
 
-import './app.css';
-import 'font-awesome/css/font-awesome.css';
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>Hello world</div>
+			);
+	}
+}
+
+const routes = (
+	<Router>
+		<Route path="/" component={Home} />
+	</Router>
+	);
+
+ReactDOM.render(<App history={browserHistory} routes={routes}/>, mountNode);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-
-import styles from './styles.module.css';
+import React, { PropTypes } from 'react';
+import { Router } from 'react-router';
 
 export default class App extends React.Component {
+	static propTypes = {
+		routes: PropTypes.object.isRequired,
+		history: PropTypes.object.isRequired
+	};
+
+	get content() {
+		return (
+			<Router routes={this.props.routes} history={this.props.history} />
+			);
+	}
+
 	render() {
 		return (
-			<div className={styles.wrapper}>
-				<h1>
-					<i className="fa fa-star"></i>
-					Enviroment: {__NODE_ENV__}
-				</h1>
-			</div>
+			<div style={{height: '100%'}}>{this.content}</div>
 			);
 	}
 }
