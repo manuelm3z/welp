5b72e413e9f4474c385868ec407b863019d52c51
Manuel Zambrano <mmzmm3z@gmail.com>
2017-01-21 23:31:12 -0300

    Some logic with de google helper in onReady function

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Map, { GoogleApiWrapper } from 'google-maps-react';
+import { searchNearby } from 'utils/googleApiHelpers';
 
 export class Container extends React.Component {
+	onReady(mapProps, map) {
+		const { google } = this.props;
+
+		const opts = {
+			location: map.center,
+			radius: '500',
+			types: ['cafe']
+		};
+
+		searchNearby(google, map, opts)
+			.then((results, pagination) => {
+				// We got some results and a pagination object
+			}).catch((status, result) => {
+				// There was an error
+			});
+	}
+
 	render() {
 		return (
 			<div>
 				Hello from the container
-				<Map google={this.props.google} />
+				<Map google={this.props.google} onReady={this.onReady.bind(this)} />
 			</div>
 			);
 	}
